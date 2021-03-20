38eea910154954f7c8acc450da9b35c9e0fef881
Whats-Gucci <72870014+Whats-Gucci@users.noreply.github.com>
2021-03-20 11:50:52 +0500

    Add files via upload

---
+++ api.php
@@ -0,0 +1,29 @@
+<?php
+
+$pdo = new PDO(
+    'mysql:host=localhost;dbname=ads_db',
+    'root','root');
+
+if (isset($_GET['add'])) {
+
+    $text = $_POST['text'];
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    
+    $query = $pdo -> prepare('INSERT INTO ads 
+    (text, name, phone) 
+    value (:text, :name, :phone)');
+    $query -> bindValue(':text', $text);
+    $query -> bindValue(':name', $name);
+    $query -> bindValue(':phone', $phone);
+    $query -> execute();
+}
+
+else if (isset($_GET['all'])) {
+
+    $query = $pdo -> query("SELECT * FROM ads");
+    $query = $query -> fetchAll(PDO::FETCH_ASSOC);
+    header("Content-type: application/json; charset=utf-8");
+
+    echo json_encode($query);
+}
---
+++ script.js
@@ -0,0 +1,109 @@
+$(document).ready(function() {
+
+    var arrayAds = new Array();
+
+    $("#add").click(function() {
+        
+        $("#modal").show();
+    });
+
+    $("#close").click(function() {
+        
+        $("#modal").hide();
+    });
+
+
+
+
+
+
+
+
+    // $("#new").click(function() {
+       
+    //      var ad = {
+    //         text: $("input[name='text']").val(),
+    //         contactName: $("input[name='name']").val(),
+    //         contactPhone: $("input[name='phone']").val()
+    //     }
+
+    //     arrayAds.push(ad);
+
+    //     console.log(arrayAds);
+    
+    //     renderAds();
+
+    $("#new").click(function() {
+       
+    //     var ad = {
+    //        text: $("input[name='text']").val(),
+    //        contactName: $("input[name='name']").val(),
+    //        contactPhone: $("input[name='phone']").val()
+    //    }
+
+    //    arrayAds.push(ad);
+
+    //    console.log(arrayAds);
+   
+    //    renderAds();
+
+            var ad = {
+           text: $("input[name='text']").val(),
+           name: $("input[name='name']").val(),
+           phone: $("input[name='phone']").val()
+       }
+
+        $.ajax({
+            type: 'POST',
+            url: 'http://localhost/api.php?add',
+            data: ad,
+            succes: function(data) {
+                initPage();
+            }
+        })
+
+        $("#modal").hide();
+    });
+
+
+    function renderAds() {
+        $("#content").html('');
+
+        arrayAds.map(ad => {
+            var adHTML = 
+            '<div class="Ads__box">' +
+                '<div class="Box__info">' + ad.text + '</div>' +
+                '<div class="Box__name">' + ad.contactName + '</div>' +
+                '<div class="Box__phone">' + ad.contactPhone + '</div>' +
+            '</div>';
+        
+            $("#content").append(adHTML);
+        });
+    }
+
+
+    init();
+
+    function init() {
+
+        $.ajax({
+            type: 'GET',
+            url: 'http://localhost/api.php?all',
+            success: function(data) {
+                console.log(data);
+
+                data.map(element => {
+                    console.log(element);
+                    var ad = {
+                        text: element.text,
+                        contactName: element.name,
+                        contactPhone: element.phone
+                    }
+
+                    arrayAds.push(ad);
+                    renderAds();
+                });
+            }
+        });
+    }
+});
